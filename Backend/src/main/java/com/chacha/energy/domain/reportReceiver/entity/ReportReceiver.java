a3a759aaec9678ab3f84ccca656f7d3052c4bc28
이정은 <silver36c@gmail.com>
2024-07-11 05:57:38 +0900

    🔧 Chore: 배포를 위해 작성 중인 코드 주석 처리

--- Backend/src/main/java/com/chacha/energy/domain/reportReceiver/entity/ReportReceiver.java
+++ Backend/src/main/java/com/chacha/energy/domain/reportReceiver/entity/ReportReceiver.java
@@ -1,24 +1,35 @@
-package com.chacha.energy.domain.reportReceiver.entity;
-
-import com.chacha.energy.common.entity.BaseEntity;
-import com.chacha.energy.domain.member.entity.Member;
-import com.chacha.energy.domain.report.entity.Report;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-
-import java.time.LocalDateTime;
-
-public class ReportReceiver extends BaseEntity {
-
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
-
-    @ManyToOne
-    private Member receiver;
-
-    @ManyToOne
-    @JoinColumn(name = "신고 내용")
-    private Report report;
-
-}
+//package com.chacha.energy.domain.reportReceiver.entity;
+//
+//import com.chacha.energy.common.entity.BaseEntity;
+//import com.chacha.energy.domain.member.entity.Member;
+//import com.chacha.energy.domain.report.entity.Report;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.Table;
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "report_receiver")
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class ReportReceiver extends BaseEntity {
+//
+//// TODO: 주석 삭제
+//// BaseEntity에 createdTime, updatedTime 있어요~
+////    private LocalDateTime createdAt;
+////
+////    private LocalDateTime updatedAt;
+//
+//    @ManyToOne
+//    private Member receiver;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "신고 내용")
+//    private Report report;
+//
+//}
